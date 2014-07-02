1d866a0ac1924d763f136de3ad15dec472288c32
odino <alessandro.nadalin@gmail.com>
2014-07-02 17:45:17 +0400

    renamed the process name to 'nikki', it's configurable, closes #11

--- server/index.js
+++ server/index.js
@@ -1,27 +1,25 @@
-var app         = require('http').createServer(handler)
-var argv        = require('yargs').argv;
-var _           = require('lodash');
-var path        = require('path');
-var staticproxy = require('./static-proxy');
-var socket      = require('./socket');
-var serveIndex  = require('./serve-index');
-var utils       = require('./utils');
-var config      = require('./config');
-var prettyjson  = require('prettyjson');
-var blankline   = console.log;
+var app             = require('http').createServer(handler)
+var argv            = require('yargs').argv;
+var _               = require('lodash');
+var path            = require('path');
+var staticproxy     = require('./static-proxy');
+var socket          = require('./socket');
+var serveIndex      = require('./serve-index');
+var utils           = require('./utils');
+var config          = require('./config');
+var prettyjson      = require('prettyjson');
+var blankline       = console.log;
+var port            = argv.port || config.get('app.port');
+var host            = argv.host || config.get('app.host');
+var defaultProject  = config.get('projects.default');
 
-var port    = argv.port || config.get('app.port') || 9123;
-var host    = argv.host || config.get('app.host') || 'localhost';
-
-var defaultProject = config.get('projects.default');
+process.title = config.get('general.process-name');
 
 if (defaultProject === 'cwd') {
     defaultProject = process.cwd();
 }
 
 var url     = 'http://' + host + ':' + port + '/' + defaultProject;
-
-
 var open    = _.has(argv, 'open') ? argv.open : config.get('app.open');
 
 app.listen(port);
