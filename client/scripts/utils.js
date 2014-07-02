a4b53964e442106e68d8841d61e38e17aca3938c
odino <alessandro.nadalin@gmail.com>
2014-07-02 17:55:14 +0400

    Now you can map file estensions to syntax highlighters from the config, closes #4

--- client/scripts/utils.js
+++ client/scripts/utils.js
@@ -1,32 +1,10 @@
 var path    = require('path');
-
-var languageGuesserMap = {
-    json:       'json',
-    markdown:   'markdown',
-    md:         'markdown',
-    yml:        'yaml',
-    js:         'javascript',
-    coffee:     'coffescript',
-    html:       'html',
-    rb:         'ruby',
-    php:        'php',
-    sql:        'sql',
-    py:         'python',
-    java:       'java',
-    css:        'css',
-    scss:       'scss',
-    c:          'c',
-    xml:        'xml'
-}
+var config  = require('./config');
 
 module.exports = {
     guessLanguage: function(filename) {
         var extension = path.extname(filename).substring(1);
 
-        if (languageGuesserMap[extension]) {
-            return languageGuesserMap[extension];
-        }
-
-        return 'text';
+        return config.get('editor.extensions')[extension] || 'text';
     }
 }
