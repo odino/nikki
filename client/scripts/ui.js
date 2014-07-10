7a77481066af0acc160bf2256fe14fc84159dd93
odino <alessandro.nadalin@gmail.com>
2014-07-11 02:29:52 +0400

    indentation

--- client/scripts/ui.js
+++ client/scripts/ui.js
@@ -9,41 +9,41 @@ var fs              = require('./fs');
  * @type {{openFile: openFile, resources: resources, bar: bar}}
  */
 var ui = {
-    /**
-     * Opens a file in the editor tab.
-     *
-     * @param resource
-     */
-    openFile: function(resource) {
-        $('.resource.open').removeClass('open');
-        $('.resource.active').addClass('open');
-        $('#subject span').remove();
-        fs.buildTitle(resource.parent);
-        $('#subject #navigation').append('<span class="highlight">/' + resource.name + '</span>');
-        state.switchFocus('tab');
-        $('#file').attr('filename', resource.path);
-        editor.openFile(resource);
-    },
-    /**
-     * Returns all the resources.
-     *
-     * @returns {*|jQuery|HTMLElement}
-     */
-    resources: function(){
-        return $('.resource');
-    },
-    /**
-     * Returns the bar.
-     *
-     * @returns {*|jQuery|HTMLElement}
-     */
-    bar: function() {
-        return $('#bar');
-    }
+  /**
+   * Opens a file in the editor tab.
+   *
+   * @param resource
+   */
+  openFile: function(resource) {
+    $('.resource.open').removeClass('open');
+    $('.resource.active').addClass('open');
+    $('#subject span').remove();
+    fs.buildTitle(resource.parent);
+    $('#subject #navigation').append('<span class="highlight">/' + resource.name + '</span>');
+    state.switchFocus('tab');
+    $('#file').attr('filename', resource.path);
+    editor.openFile(resource);
+  },
+  /**
+   * Returns all the resources.
+   *
+   * @returns {*|jQuery|HTMLElement}
+   */
+  resources: function(){
+    return $('.resource');
+  },
+  /**
+   * Returns the bar.
+   *
+   * @returns {*|jQuery|HTMLElement}
+   */
+  bar: function() {
+    return $('#bar');
+  }
 };
 
 events.on('resource.opened', function(resource){
-    ui.openFile(resource);
+  ui.openFile(resource);
 });
 
 module.exports = ui;
