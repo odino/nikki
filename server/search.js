657b7b48f23fb3d0a2f10dea6a0fe7813f083409
odino <alessandro.nadalin@gmail.com>
2014-07-11 15:28:12 +0400

    indentation

--- server/search.js
+++ server/search.js
@@ -6,9 +6,9 @@
  */
 var p       = require('path');
 var _       = require('lodash');
+var fs      = require('fs');
 var debug   = require('debug')('nikki:search');
 var config  = require('./config');
-
 var search = {};
 
 /**
@@ -28,17 +28,17 @@ var search = {};
  * @returns {*}
  */
 search.getRegex = function(text) {
-    var regexString = '';
+  var regexString = '';
 
-    text.split(' ').forEach(function(word){
-        word = word.trim();
+  text.split(' ').forEach(function(word){
+    word = word.trim();
 
-        if (word) {
-            regexString += "(.*)(" + word + ")"
-        }
-    });
+    if (word) {
+      regexString += "(.*)(" + word + ")"
+    }
+  });
 
-    return RegExp(regexString, "ig");
+  return RegExp(regexString, "ig");
 };
 
 /**
@@ -51,40 +51,40 @@ search.getRegex = function(text) {
  * @param socket
  */
 search.find = function(options, socket) {
-    var notifyResult = function(resource){
-        socket.emit('search.result', resource)
-        debug('search result ', resource);
-    };
-    
-    var finder = require('findit')(options.root.path);
-    
-    finder.on('directory', function (dir, stat, stop) {
-        var base = p.basename(dir);
+  var notifyResult = function(resource){
+      socket.emit('search.result', resource)
+      debug('search result ', resource);
+  };
+  
+  var finder = require('findit')(options.root.path);
+  
+  finder.on('directory', function (dir, stat, stop) {
+    var base = p.basename(dir);
 
-        if (_.contains(config.get('search.exclude'), base)) {
-          stop();
-        } else {
-          if (dir.match(search.getRegex(options.text))) {
-            notifyResult({
-              path: dir,
-              type: 'directory',
-              name: base,
-              parent: p.dirname(dir)
-            });
-          }
-        }
-    });
-    
-    finder.on('file', function (file, stat) {
-        if (file.match(search.getRegex(options.text))) {
-          notifyResult({
-            path: file,
-            type: 'file',
-            name: p.basename(file),
-            parent: p.dirname(file)
-          });
-        }
-    });
+    if (_.contains(config.get('search.exclude'), base)) {
+      stop();
+    } else {
+      if (dir.match(search.getRegex(options.text))) {
+        notifyResult({
+          path: dir,
+          type: 'directory',
+          name: base,
+          parent: p.dirname(dir)
+        });
+      }
+    }
+  });
+  
+  finder.on('file', function (file, stat) {
+    if (file.match(search.getRegex(options.text))) {
+      notifyResult({
+        path: file,
+        type: 'file',
+        name: p.basename(file),
+        parent: p.dirname(file)
+      });
+    }
+  });
 };
 
 module.exports = search;
